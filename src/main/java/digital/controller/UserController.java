ea377bea8005af07e194637a61fa7642095cd033
bloss <1248212664@qq.com>
2018-04-07 02:39:15 +0800

    fixs controller mapping path

--- src/main/java/digital/controller/UserController.java
+++ src/main/java/digital/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/dglib/user")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
